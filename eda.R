270fdc21af736f2ad22726f7ebfce2ea6502c3b6
Peter Bösenberg <peterboesenberg@gmail.com>
2020-07-12 17:09:27 +0200

    check for normal distribution

--- eda.R
+++ eda.R
@@ -1,8 +1,25 @@
 library(corrplot)
 library(modules)
+library(ggpubr)
 load <- modules::use("load.R")
+clean <- modules::use("clean.R")
 
 train <- load$load_train()
+train <- clean$clean(train)
 
-res <- cor(train, method = c("pearson", "kendall", "spearman"))
-corrplot(res, type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+check_correlations <- function(data) {
+  res <- cor(data, method = c("pearson", "kendall", "spearman"))
+  corrplot(res, type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+}
+
+check_normal_distribution <- function(data) {
+  dist_test <- sapply(data, shapiro.test)
+  dist_test <- as.data.table(t(as.data.table(dist_test, keep.rownames = TRUE)[2]), keep.rownames = TRUE)[-1]
+  dist_test[, is_normal:=V1>0.05]
+  dist_test
+}
+
+
+check_correlations(train)
+distributions <- check_normal_distribution(train)
+print(distributions)
